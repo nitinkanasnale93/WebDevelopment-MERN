f185d86924b6c61702c5dbf9ffca8f737157029e
Nitin <Nitinkanasnale3@gmail.com>
2026-09-17 23:13:35 +0530

    yet to complete

---
+++ 03-JavaScript/Lec03/index.js
@@ -0,0 +1,27 @@
+// let password = prompt("Enter your password: ");
+// let newPass = password.trim();
+// console.log(newPass);
+
+// let msg = "apna   ";
+// console.log("Original:", msg);
+// console.log("After trim:", msg.trim());
+// let str = msg.trim();
+// console.log("Stored trimmed value:", str);
+// console.log("Original msg:", msg);
+// msg = "hello";
+// console.log("New msg:", msg);
+
+// let name = "Vishwa";
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+// let str = "ILoveCoding";
+
+// console.log(str.indexOf("Love"));
+// console.log(str.indexOf("J"));
+// console.log(str.indexOf("o"));
+
+let msg = "Nitin   ";
+let newmsg = msg.trim();
+console.log("After Trimming: ", newmsg);
+console.log("UpperCase: ", msg.toUpperCase());
