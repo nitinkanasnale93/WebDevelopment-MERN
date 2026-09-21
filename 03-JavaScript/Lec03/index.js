75e2b8a7745bbf9b635d4dd47a009131491596df
Nitin <Nitinkanasnale3@gmail.com>
2026-09-21 23:43:52 +0530

    Just a break for 2 days Reason: NPTEL & IBM

--- 03-JavaScript/Lec03/index.js
+++ 03-JavaScript/Lec03/index.js
@@ -21,7 +21,43 @@
 // console.log(str.indexOf("J"));
 // console.log(str.indexOf("o"));
 
-let msg = "Nitin   ";
-let newmsg = msg.trim();
-console.log("After Trimming: ", newmsg);
-console.log("UpperCase: ", msg.toUpperCase());
+// let msg = "Nitin   ";
+// let newmsg = msg.trim();
+// console.log("After Trimming: ", newmsg);
+// console.log("UpperCase: ", msg.toUpperCase());
+
+// let msg = "Hello";
+// console.log(msg.slice(0, 4));
+
+// let msg1 = "NationalServiceScheme";
+// console.log(msg1.slice(8, msg1.length - 1));
+// console.log(msg1.slice(-1)); //-2 it'll print em like this
+
+// let msg = "Love";
+// console.log(msg.replace("Love", "do"));
+// console.log(msg.replace('o', 'x'));
+// console.log(msg.repeat(3));
+
+//Practice Qs
+// let msg = "Help!"
+//console.log(msg.trim().toUpperCase());
+
+// let name = "SindhiCollege";
+// console.log(name.slice(6));
+// console.log(name.indexOf("Co"));
+// console.log(name.replace("Sindhi", "Presidency"));
+// // console.log(name.slice(6).replace('l', 't'));
+// let students = ["Kannika", "Nitin", "Raju"];
+
+// console.log(students[0]);
+// console.log(students.length);
+// console.log(typeof students);
+// console.log(students[0][0]);
+
+// //Array - we can store diffrent type of values also like:
+// let info = ["Nitin", 21, 70.0]
+
+let fruits = ["Mango", "Apple", "Litchi"];
+fruits[0] = "Banana";
+fruits[10] = "Papaya";
+console.log(fruits);
